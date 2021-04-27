d04f998ef5bb47ed90f1cd906d2939aae48d140a
oscardhc03 <60898622+oscardhc03@users.noreply.github.com>
2021-04-26 20:53:18 -0500

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -26,6 +26,17 @@ localparam R_Type_NOR    = 9'b111_100111;
 localparam R_Type_ADD    = 9'b111_100000;
 localparam I_Type_ADDI   = 9'b100_xxxxxx;
 localparam I_Type_ORI    = 9'b101_xxxxxx;
+localparam R_Type_SUB	 = 9'b111_100010;
+localparam R_Type_SLL	 = 9'b111_000000;
+localparam R_Type_SRL	 = 9'b111_000010;
+localparam I_Type_ADDI   = 9'b000_xxxxxx;
+localparam I_Type_ORI    = 9'b001_xxxxxx;
+localparam I_Type_LUI	 = 9'b010_xxxxxx;
+localparam I_Type_ANDI	 = 9'b011_xxxxxx;
+localparam I_Type_BEQ	 = 9'b100_xxxxxx;
+localparam I_Type_LW		 = 9'b101_xxxxxx;
+localparam I_Type_SW		 = 9'b110_xxxxxx;
+
 
 reg [3:0] ALUControlValues;
 wire [8:0] Selector;
